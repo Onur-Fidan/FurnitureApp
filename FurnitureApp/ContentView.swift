6f8d69e5cf962cf0bfce0152982bd2b0c0c19a6a
Onur Fidan <97627723+Onurfdn@users.noreply.github.com>
2023-08-26 20:57:29 +0300

    Initial Commit

---
+++ FurnitureApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FurnitureApp
+//
+//  Created by Onur Fidan on 26.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            HomeScreen()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Onur Fidan on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
